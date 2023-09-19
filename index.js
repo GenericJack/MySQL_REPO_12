5e83c0821728124a2c3b6bc745e3a80307b87960
John Simeoni <jack.simeoni@gmail.com>
2023-09-19 17:03:36 -0400

    added functions and created tables: function add departments, employees, etc.

---
+++ index.js
@@ -0,0 +1,63 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+//establish connection
+const connection = sql.createConnection({
+    host: 'localhost',
+    root: 'root',
+    password: 'password',
+    database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+});
+
+//attempt to create functions for options
+function viewDepartments() {
+    const query = 'SELECT * FROM department';
+    connection.query(query, (err, results) => {
+        if (err) throw err;
+        //display the results in a table format
+        console.table(results);
+        //call function to display the main menu again
+        mainMenu();
+    });
+}
+
+function mainMenu() {
+    inquirer
+        .createPromptModule([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What can I do for you?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    //placeholder for more options
+                ],
+            },
+        ])
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+                    case 'View all roles':
+                        //call function to view all roles
+                        break;
+
+                    case 'View all employees':
+                        break;
+                    case 'Add a department':
+                        break;
+                    default:
+                        connection.end(); //close database connection
+                        console.log('Until next time!');
+            }
+        });
+}
